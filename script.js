8765bd12352f78be1ccd483fc3f4f6cc6168df21
natalyamyunster <natalyamyunster@yahoo.com>
2021-02-16 10:49:45 +0100

    hw2

--- script.js
+++
@@ -1,6 +0,0 @@
-let num = 0;
-let score = 0;
-const question1 = {};
-const question2 = {};
-const question3 = {};
-const question4 = {};
---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+let num = '0';
+let score;
+const question1 = {
+    question: 'When JS was created and Hwo created it?  ',
+    answer: 'JS was created in 1995 by Brendan Eich'
+};
+const question2 = {
+    question: 'What is JavaScript?',
+    answer: 'JavaScript is a client-side as well as server side scripting language that can be inserted into HTML pages and is understood by web browsers. JavaScript is also an Object based Programming language.'
+};
+const question3 = {
+    question: 'Are Java and JavaScript same?',
+    answer: 'No, Java and JavaScript are the two different languages. Java is a robust, secured and object-oriented programming language whereas JavaScript is a client side scripting language with some limitations.'
+};
+const question4 = {
+    question: 'What is ‘this’ keyword in JavaScript?',
+    answer: '‘This’ keyword refers to the object from where it was called.'
+};
