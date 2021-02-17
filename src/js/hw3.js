02d0226352fa9f53ab99878380f1d604cfe80a8a
natalyamyunster <natalyamyunster@yahoo.com>
2021-02-17 21:59:15 +0100

    hw3

---
+++ src/js/hw3.js
@@ -0,0 +1,31 @@
+//задание 1
+let a = 1;
+if ( a === 0 ) {
+    console.log('верно');
+} else {
+    console.log('неверно');
+}
+
+let b = 0;
+if ( b === 0 ) {
+    console.log('верно');
+} else {
+    console.log('неверно');
+}
+
+let c = -3;
+if ( c === 0 ) {
+    console.log('верно');
+} else {
+    console.log('неверно');
+}
+
+//задание 2
+
+for (let d = 1; d <= 20; d++) {
+    if (d % 2 === 0) {
+        console.log(d + ', переменная - четная');
+    } else {
+        console.log(d + ', переменная - нечетная');
+    }
+}
